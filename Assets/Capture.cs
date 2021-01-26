04a5f8b8391c5d93943c4e69295d8c16ab0d3300
Paul Sinnett <paul.sinnett@gmail.com>
2021-01-26 19:31:11 +0000

    testing render to texture on Web GL

---
+++ Assets/Capture.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Capture : MonoBehaviour
+{
+    public Camera captureCamera;
+    public RenderTexture renderTexture;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            StartCoroutine(CaptureImage());
+        }
+    }
+
+    IEnumerator CaptureImage()
+    {
+        captureCamera.targetTexture = renderTexture;
+        yield return null;
+        captureCamera.targetTexture = null;
+    }
+}
